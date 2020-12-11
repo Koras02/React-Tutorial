99703322dbed91b22baf208da8689c21d0408ccf
Koras02 <wjdgh0727@gmail.com>
2020-12-12 01:59:41 +0900

    Props

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function getGreeting(user){
+  if(user) {
+    return <h1>Hello {formatName(user)}!</h1>
+  }
+  return <h1>Hello, Stranger.</h1>
+}
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName; 
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+}
+
+const element = (
+   <h1>
+     Hello, {getGreeting(user)}
+     Hello, {formatName(user)}!
+    </h1>
+);
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
